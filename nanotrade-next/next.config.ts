35187d534118d164a274b5518ae1139401add333
seeron6 <seeronsiva6@gmail.com>
2026-02-21 02:24:11 -0500

    net config

--- nanotrade-next/next.config.ts
+++ nanotrade-next/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/NanoTrade-website', // This tells Next.js where the site lives
   images: {
     unoptimized: true,
   },
